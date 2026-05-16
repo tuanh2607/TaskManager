1273437ed95e9c636d1a4298ca9b278cf66a7de0
tuan anh <tvtanh2607@gmail.com>
2026-05-16 08:43:14 +0000

    add

---
+++ lib/HashTable.hpp
@@ -0,0 +1,29 @@
+#include "AVL.hpp"
+#include <vector>
+template<typename A, typename B>
+class HashTable {
+private:
+    vector<pair<A,B>> table;
+    int hashFunc(A key) {
+        
+    }
+public:
+    void insert(A value, B, key) {
+
+    }
+    void remove(A key) {
+
+    }
+    B find(A key) {
+
+    }
+    bool contains(A key) {
+
+    }
+    int size() {
+
+    }
+    void clear(){
+
+    }
+};
